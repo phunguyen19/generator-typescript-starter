b47c46b98203b049ba9364b7fea3ade5d377e611
Phu Nguyen <phu.nguyen19@gmail.com>
2019-04-27 14:56:05 +0700

    Modular core files and packages

--- app/index.js
+++ app/index.js
@@ -1,47 +1,55 @@
 'use strict';
 const fs = require('fs');
 const Generator = require('yeoman-generator');
+
+const corePkg = ['dotenv-save'];
+const coreDevPkg = ['pre-commit'];
+const minimalDevPkg = ['@types/jest', '@types/node', 'jest', 'ts-jest', 'ts-node-dev', 'tslint-eslint-rules', 'tslint', 'typescript'];
+
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     this.argument('framework', { type: String, required: false });
-    this.argument('fr', { type: String, required: false });
-    this.log('Initializing...');
   }
 
   start() {
+    const framework = this.options.framework || this.options.fr || 'minimal';
+
+    if (!fs.existsSync(this.templatePath(framework))) {
+      /* eslint-disable no-console */
+      return console.log(`Only support frameworks: minimal, nestjs`);
+      /* eslint-enable no-console */
+    }
+
+    this.log('Initializing...');
+
     this.prompt([
       {
         type: 'input',
         name: 'name',
-        message: 'Enter a name for the new project: '
+        message: 'Project name: ',
+        default: 'typescript-stater-' + +new Date()
       }
     ]).then(answers => {
-      const framework = this.options.framework || this.options.fr || 'minimal';
-
-      if (!fs.existsSync(this.templatePath(framework))) {
-        throw new Error(`Support frameworks: minimal, nestjs`);
-      }
-
       // create destination folder
       this.destinationRoot(answers.name);
       this.spawnCommandSync('git', ['init']);
+
+      this.fs.copyTpl(this.templatePath(`core/**/*`), this.destinationPath(this.destinationRoot()), answers, undefined, {
+        globOptions: { dot: true }
+      });
 
       this.fs.copyTpl(this.templatePath(`${framework}/**/*`), this.destinationPath(this.destinationRoot()), answers, undefined, {
         globOptions: { dot: true }
       });
 
+      this.npmInstall();
+      this.npmInstall(corePkg);
+      this.npmInstall(coreDevPkg, { 'save-dev': true });
+
       switch (framework) {
         case 'minimal':
-          this.npmInstall(
-            ['@types/jest', '@types/node', 'dotenv', 'jest', 'pre-commit', 'ts-jest', 'ts-node-dev', 'tslint-eslint-rules', 'tslint', 'typescript'],
-            { 'save-dev': true }
-          );
-          break;
-
-        case 'nestjs':
-          this.npmInstall();
-          this.npmInstall(['pre-commit'], { 'save-dev': true });
+          this.npmInstall(minimalDevPkg, { 'save-dev': true });
           break;
       }
     });

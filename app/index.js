7fda2088d72a3290f9ffb6b05df767ec97ca4f85
Phu Nguyen <phu.nguyen19@gmail.com>
2019-05-10 18:54:08 +0700

    Moving all dependencies package to package.json

--- app/index.js
+++ app/index.js
@@ -2,10 +2,6 @@
 const fs = require('fs');
 const Generator = require('yeoman-generator');
 
-const corePkg = ['dotenv-safe'];
-const coreDevPkg = ['pre-commit'];
-const minimalDevPkg = ['@types/jest', '@types/node', 'jest', 'ts-jest', 'ts-node-dev', 'tslint-eslint-rules', 'tslint', 'typescript'];
-
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
@@ -24,40 +20,28 @@ module.exports = class extends Generator {
 
     const questions = [];
 
-    if (!this.options.name) questions.push({
-      type: 'input',
-      name: 'name',
-      message: 'Project name: ',
-      default: 'typescript-stater-' + +new Date()
-    });
+    if (!this.options.name)
+      questions.push({
+        type: 'input',
+        name: 'name',
+        message: 'Project name: ',
+        default: 'typescript-stater-' + +new Date()
+      });
 
     this.prompt(questions).then(answers => {
       const params = Object.assign({}, this.options, answers);
-
+      
       // create destination folder
       this.destinationRoot(params.name);
+      const destination = this.destinationPath(this.destinationRoot());
+      const copyTplOptions = { globOptions: { dot: true } };
+      
       this.spawnCommandSync('git', ['init']);
 
-      this.fs.copyTpl(this.templatePath(`core/**/*`), this.destinationPath(this.destinationRoot()), params, undefined, {
-        globOptions: { dot: true }
-      });
-
-      this.fs.copyTpl(this.templatePath(`${framework}/**/*`), this.destinationPath(this.destinationRoot()), params, undefined, {
-        globOptions: { dot: true }
-      });
+      this.fs.copyTpl(this.templatePath(`core/**/*`), destination, params, undefined, copyTplOptions);
+      this.fs.copyTpl(this.templatePath(`${framework}/**/*`), destination, params, undefined, copyTplOptions);
 
       this.npmInstall();
-      this.npmInstall(corePkg);
-      this.npmInstall(coreDevPkg, { 'save-dev': true });
-
-      switch (framework) {
-        case 'minimal':
-          this.npmInstall(minimalDevPkg, { 'save-dev': true });
-          break;
-        case 'nestjs':
-          this.npmInstall(['pg', 'sequelize', 'sequelize-typescript']);
-          this.npmInstall(['@types/sequelize'], { 'save-dev': true });
-      }
-    })
+    });
   }
 };

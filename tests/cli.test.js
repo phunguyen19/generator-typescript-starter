a331ac6bb33108c0a00b492a3ea37192717c7da7
Phu Nguyen <phu.nguyen19@gmail.com>
2019-07-11 22:41:21 +0700

    Change test runner to be ava

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -1,29 +1,31 @@
 const path = require('path');
 const helpers = require('yeoman-test');
 const assert = require('yeoman-assert');
+const test = require('ava');
 
-it('generate new cli project successful', () => {
+test('generate new cli project successful', async t => {
   // The object returned acts like a promise, so return it to wait until the process is done
-  return helpers
+  await helpers
     .run(path.join(__dirname, '../app'))
     .withPrompts({
       name: 'test-project',
       framework: 'cli',
-    })
-    .then(function() {
-      assert.file([
-        '.git/',
-        '.vscode/launch.json',
-        '.vscode/settings.json',
-        'src/',
-        '.editorconfig',
-        '.env.example',
-        '.gitignore',
-        '.prettierrc',
-        'package.json',
-        'README.md',
-        'tsconfig.json',
-        'tslint.json'
-      ]);
     });
+
+    assert.file([
+      '.git/',
+      '.vscode/launch.json',
+      '.vscode/settings.json',
+      'src/',
+      '.editorconfig',
+      '.env.example',
+      '.gitignore',
+      '.prettierrc',
+      'package.json',
+      'README.md',
+      'tsconfig.json',
+      'tslint.json'
+    ]);
+    
+    t.pass();
 });
--- tests/nestjs.test.js
+++ tests/nestjs.test.js
@@ -1,34 +1,37 @@
 const path = require('path');
 const helpers = require('yeoman-test');
 const assert = require('yeoman-assert');
+const test = require('ava');
 
-it('generate new nestjs project successful', () => {
+test('generate new nestjs project successful', async t => {
   // The object returned acts like a promise, so return it to wait until the process is done
-  return helpers
+  await helpers
     .run(path.join(__dirname, '../app'))
     .withPrompts({
       name: 'test-project',
       framework: 'nestjs',
     })
-    .then(function() {
-      assert.file([
-        '.git/',
-        '.vscode/launch.json',
-        'src/',
-        'tests/',
-        '.editorconfig',
-        '.env',
-        '.gitignore',
-        '.prettierrc',
-        'docker-compose.yml',
-        'Dockerfile.prod',
-        'Dockerfile',
-        'package.json',
-        'README.md',
-        'tsconfig.json',
-        'tslint.json'
-      ]);
-      assert.fileContent('README.md', /test-project/);
-      assert.fileContent('package.json', /"name": "test-project"/);
-    });
+
+    assert.file([
+      '.git/',
+      '.vscode/launch.json',
+      'src/',
+      'tests/',
+      '.editorconfig',
+      '.env',
+      '.gitignore',
+      '.prettierrc',
+      'docker-compose.yml',
+      'Dockerfile.prod',
+      'Dockerfile',
+      'package.json',
+      'README.md',
+      'tsconfig.json',
+      'tslint.json'
+    ]);
+
+    assert.fileContent('README.md', /test-project/);
+    assert.fileContent('package.json', /"name": "test-project"/);
+
+    t.pass();
 });

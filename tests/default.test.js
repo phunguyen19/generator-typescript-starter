f322601642a52b2f315e2ed0a2a3e2f76e1b2465
Richard <phu.nguyen19@gmail.com>
2020-11-06 12:05:44 +0700

    #9 Deprecated ts-lint and update packages

--- tests/default.test.js
+++ tests/default.test.js
@@ -3,33 +3,32 @@ const helpers = require('yeoman-test');
 const assert = require('yeoman-assert');
 const test = require('ava');
 
-test('minimal is default template', async t => {
-  await helpers
-    .run(path.join(__dirname, '../app'))
-    .withPrompts({
-      name: 'test-project',
-    });
-    
-    assert.file([
-      '.git/',
-      '.vscode/launch.json',
-      'src/',
-      'tests/',
-      '.editorconfig',
-      '.env',
-      '.gitignore',
-      '.prettierrc',
-      'docker-compose.yml',
-      'Dockerfile.prod',
-      'Dockerfile',
-      'package.json',
-      'README.md',
-      'tsconfig.json',
-      'tslint.json'
-    ]);
+test('minimal is default template', async (t) => {
+  await helpers.run(path.join(__dirname, '../app')).withPrompts({
+    name: 'test-project',
+  });
 
-    assert.fileContent('README.md', /test-project/);
-    assert.fileContent('package.json', /"name": "test-project"/);
+  assert.file([
+    '.editorconfig',
+    '.eslintignore',
+    '.eslintrc',
+    '.git/',
+    '.gitignore',
+    '.prettierrc',
+    '.vscode/launch.json',
+    'docker-compose.yml',
+    'Dockerfile.prod',
+    'Dockerfile',
+    'package.json',
+    'README.md',
+    'src/',
+    'tsconfig.json',
+  ]);
 
-    t.pass();
+  assert.noFile(['tslint.json']);
+
+  assert.fileContent('README.md', /test-project/);
+  assert.fileContent('package.json', /"name": "test-project"/);
+
+  t.pass();
 });
--- tests/minimal.test.js
+++ tests/minimal.test.js
@@ -3,7 +3,7 @@ const helpers = require('yeoman-test');
 const assert = require('yeoman-assert');
 const test = require('ava');
 
-test('generate new minimal project successful', async t => {
+test('generate new minimal project successful', async (t) => {
   // The object returned acts like a promise, so return it to wait until the process is done
   await helpers
     .run(path.join(__dirname, '../app'))
@@ -13,27 +13,28 @@ test('generate new minimal project successful', async t => {
     .withPrompts({
       name: 'test-project',
     });
-    
-    assert.file([
-      '.git/',
-      '.vscode/launch.json',
-      'src/',
-      'tests/',
-      '.editorconfig',
-      '.env',
-      '.gitignore',
-      '.prettierrc',
-      'docker-compose.yml',
-      'Dockerfile.prod',
-      'Dockerfile',
-      'package.json',
-      'README.md',
-      'tsconfig.json',
-      'tslint.json'
-    ]);
 
-    assert.fileContent('README.md', /test-project/);
-    assert.fileContent('package.json', /"name": "test-project"/);
+  assert.file([
+    '.editorconfig',
+    '.eslintignore',
+    '.eslintrc',
+    '.git/',
+    '.gitignore',
+    '.prettierrc',
+    '.vscode/launch.json',
+    'docker-compose.yml',
+    'Dockerfile.prod',
+    'Dockerfile',
+    'package.json',
+    'README.md',
+    'src/',
+    'tsconfig.json',
+  ]);
 
-    t.pass();
+  assert.noFile(['tslint.json']);
+
+  assert.fileContent('README.md', /test-project/);
+  assert.fileContent('package.json', /"name": "test-project"/);
+
+  t.pass();
 });
